b92b9ec129bf16b7eb46e25138c8636e268c4e7a
Matthew Stevenson <stevensonmt@gmail.com>
2021-12-04 09:38:49 -0800

    wrong pt2 don't know why

--- 2021/day4/lib/day4.ex
+++ 2021/day4/lib/day4.ex
@@ -1,4 +1,6 @@
 defmodule Day4 do
+  @moduledoc false
+
   @input "input.txt" |> File.read!() |> String.split()
 
   @drawpool hd(@input) |> String.split(",")
@@ -6,6 +8,17 @@ defmodule Day4 do
   @boards tl(@input)
           |> Enum.chunk_every(5)
           |> Enum.chunk_every(5)
+
+  @boards2 (for board <- @boards do
+              for {row, ri} <- Enum.with_index(board) do
+                for {col, ci} <- Enum.with_index(row) do
+                  {col, {ri, ci}}
+                end
+              end
+              |> List.flatten()
+              |> Map.new()
+              |> then(&{&1, MapSet.new(), MapSet.new()})
+            end)
 
   def do_pt_1() do
     bingo(@drawpool, @boards, 1)
@@ -32,7 +45,6 @@ defmodule Day4 do
           bingo(rest, newer_boards, pt)
         rescue
           _ ->
-            IO.inspect({winner, n})
             sum_board(winner) * String.to_integer(n)
         end
 
@@ -41,8 +53,42 @@ defmodule Day4 do
     end
   end
 
-  def loser_bingo([n | rest], boards) do 
+  def loser_bingo([n | rest], boards) do
+    marked_boards = boards |> Enum.map(&mark_cells(n, &1))
 
+    checked = check_boards2(marked_boards)
+
+    case {length(marked_boards), length(checked)} do
+      {2, 1} ->
+        loser = marked_boards |> Enum.reject(&Enum.any?(checked, fn b -> b == &1 end))
+        IO.inspect(loser)
+        IO.puts("$$$$")
+        IO.inspect(marked_boards)
+        IO.puts("%%%%")
+        IO.inspect(checked)
+        sum_board(loser) |> Kernel.*(String.to_integer(n))
+
+      _ ->
+        loser_bingo(rest, checked)
+    end
+  end
+
+  defp check_boards2(boards) do
+    tmp =
+      boards
+      |> Enum.reject(&empty_rows(&1))
+
+    IO.inspect(length(tmp))
+
+    tmp2 =
+      tmp
+      |> cols_transform()
+      |> Enum.reject(&empty_cols(&1))
+
+    IO.inspect(length(tmp2))
+
+    tmp2
+    |> cols_transform()
   end
 
   defp mark_cells(n, board) do
@@ -66,9 +112,8 @@ defmodule Day4 do
     if winning_row == nil do
       winning_col =
         boards
-        |> Enum.map(&Enum.zip(&1))
-        |> Enum.map(&Enum.map(&1, fn r -> Tuple.to_list(r) end))
-        |> empty_rows()
+        |> cols_transform()
+        |> Enum.find(&empty_cols(&1))
 
       if winning_col == nil do
         nil
@@ -85,6 +130,17 @@ defmodule Day4 do
     |> Enum.find(&Enum.all?(&1, fn c -> c == nil end))
   end
 
+  defp cols_transform(boards) do
+    boards
+    |> Enum.map(&Enum.zip(&1))
+    |> Enum.map(&Enum.map(&1, fn r -> Tuple.to_list(r) end))
+  end
+
+  defp empty_cols(board) do
+    board
+    |> empty_rows()
+  end
+
   defp sum_board(board) do
     board
     |> List.flatten()
@@ -94,7 +150,7 @@ defmodule Day4 do
   end
 
   def do_pt_2() do
-    bingo(@drawpool, @boards, 2)
+    loser_bingo(@drawpool, @boards)
   end
 end
 
