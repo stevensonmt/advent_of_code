4a03dbb960900edc1be41abb52f03f4ee1cf6119
Matthew Stevenson <stevensonmt@gmail.com>
2021-12-08 13:24:25 -0800

    polishing...TODO: process input to MapSets to begin with

--- 2021/day8/lib/day8.ex
+++ 2021/day8/lib/day8.ex
@@ -1,33 +1,30 @@
 defmodule Day8 do
   @moduledoc false
 
-  @input "input.txt"
-         |> File.stream!()
-         |> Stream.map(&String.trim/1)
-         |> Stream.map(&String.split(&1, " | "))
-         |> Stream.map(&Enum.map(&1, fn s -> String.split(s) end))
-         |> Enum.to_list()
-         |> IO.inspect()
-
-  @segments [:top, :northeast, :southeast, :bottom, :southwest, :northwest, :median]
+  def input do
+    "input.txt"
+    |> File.stream!()
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.split(&1, " | "))
+    |> Stream.map(&Enum.map(&1, fn s -> String.split(s) end))
+  end
 
   @uniqdigitsegments [2, 3, 4, 7]
 
   def do_pt_1() do
-    @input
-    |> Enum.map(&decode_uniq_digits/1)
+    input()
+    |> Stream.map(&count_uniq_digits/1)
     |> Enum.sum()
   end
 
-  def decode_uniq_digits([digits, coded_vals]) do
+  def count_uniq_digits([_digits, coded_vals]) do
     coded_vals
     |> Enum.count(&Enum.any?(@uniqdigitsegments, fn i -> String.length(&1) == i end))
   end
 
   def do_pt_2() do
-    @input
-    # |> Enum.take(3)
-    |> Enum.map(fn [digits, vals] ->
+    input()
+    |> Stream.map(fn [digits, vals] ->
       [
         digits
         |> Enum.map(fn d -> String.graphemes(d) |> Enum.sort() end)
@@ -35,71 +32,82 @@ defmodule Day8 do
         vals |> Enum.map(fn d -> String.graphemes(d) |> Enum.sort() end)
       ]
     end)
-    |> Enum.map(&decipher/1)
+    |> Stream.map(&decipher/1)
     |> Enum.sum()
   end
 
   def decipher([digits, coded_vals]) do
-    cipher =
-      [{1, 2}, {7, 3}, {4, 4}, {8, 7}]
-      |> Enum.map(fn {n, segs} ->
-        {n, Enum.find(digits, fn s -> length(s) == segs end)}
-      end)
-      |> Enum.into(%{})
-
-    find_nine_six_and_zero(cipher, digits)
-    |> find_two_three_and_five(digits)
-    |> Enum.map(fn {k, v} -> {v, k} end)
-    |> Enum.into(%{})
+    digits
+    |> decode_uniq_digits()
+    |> decode_six_seg_digits(digits)
+    |> decode_five_seg_digits(digits)
+    |> Enum.reduce(%{}, fn {k, v}, acc -> Map.put(acc, v, k) end)
     |> decode_vals(coded_vals)
   end
 
-  defp find_nine_six_and_zero(map, digits) do
-    one = Map.get(map, 1)
+  defp decode_uniq_digits(digits) do
+    [1, 7, 4, 8]
+    |> Stream.zip(@uniqdigitsegments)
+    |> Stream.map(fn {n, segs} ->
+      {n, Enum.find(digits, fn s -> length(s) == segs end)}
+    end)
+    |> Enum.into(%{})
+  end
 
-    four = Map.get(map, 4)
+  defp decode_six_seg_digits(map, digits) do
+    digits
+    |> Enum.filter(fn d -> length(d) == 6 end)
+    |> decode_six(map)
+    |> decode_zero_nine(digits)
+  end
 
-    nine_six_or_zero = digits |> Enum.filter(fn d -> length(d) == 6 end)
+  defp decode_five_seg_digits(map, digits) do
+    digits
+    |> Enum.filter(fn d -> length(d) == 5 end)
+    |> decode_three(map)
+    |> decode_two_five(digits)
+  end
 
-    [six] =
-      nine_six_or_zero
-      |> Enum.reject(fn d ->
-        Enum.all?(one, fn c -> Enum.find(d, fn e -> c == e end) end)
-      end)
+  defp decode_six(candidates, map) do
+    Map.put(
+      map,
+      6,
+      candidates
+      |> Enum.find(fn d -> not MapSet.subset?(MapSet.new(Map.get(map, 1)), MapSet.new(d)) end)
+    )
+  end
 
-    [zero, nine] =
-      (nine_six_or_zero -- [six])
-      |> Enum.sort_by(fn d -> Enum.all?(four, fn e -> Enum.find(d, fn f -> f == e end) end) end)
-
-    [zero, six, nine]
-    |> Enum.zip([0, 6, 9])
+  defp decode_zero_nine(map, digits) do
+    digits
+    |> Enum.filter(fn d -> length(d) == 6 and Map.get(map, 6) != d end)
+    |> Enum.sort_by(fn d -> MapSet.subset?(MapSet.new(Map.get(map, 4)), MapSet.new(d)) end)
+    |> Enum.zip([0, 9])
     |> Enum.reduce(map, fn {v, k}, acc -> Map.put(acc, k, v) end)
   end
 
-  defp find_two_three_and_five(map, digits) do
-    one = Map.get(map, 1)
-    eight = Map.get(map, 8)
-    nine = Map.get(map, 9)
-    [southwest] = eight -- nine
-
-    candidates = digits |> Enum.filter(fn d -> length(d) == 5 end)
-
-    three =
+  defp decode_three(candidates, map) do
+    Map.put(
+      map,
+      3,
       candidates
-      |> Enum.find(fn d -> Enum.all?(one, fn c -> Enum.find(d, fn e -> c == e end) end) end)
+      |> Enum.find(fn d -> MapSet.subset?(MapSet.new(Map.get(map, 1)), MapSet.new(d)) end)
+    )
+  end
 
-    [five, two] =
-      (candidates -- [three])
-      |> Enum.sort_by(fn d -> Enum.find(d, fn e -> e == southwest end) end)
-
-    [two, three, five]
-    |> Enum.zip([2, 3, 5])
+  defp decode_two_five(map, digits) do
+    digits
+    |> Enum.filter(fn d -> length(d) == 5 and Map.get(map, 3) != d end)
+    |> Enum.sort_by(fn d ->
+      MapSet.subset?(
+        MapSet.new(MapSet.difference(MapSet.new(Map.get(map, 8)), MapSet.new(Map.get(map, 9)))),
+        MapSet.new(d)
+      )
+    end)
+    |> Enum.zip([5, 2])
     |> Enum.reduce(map, fn {v, k}, acc -> Map.put(acc, k, v) end)
   end
 
   defp decode_vals(map, coded_vals) do
-    IO.inspect(map)
-
     coded_vals
     |> Enum.map(fn c ->
       Map.get(map, c)
@@ -108,5 +116,5 @@ defmodule Day8 do
   end
 end
 
-# Day8.do_pt_1() |> IO.inspect()
+Day8.do_pt_1() |> IO.inspect()
 Day8.do_pt_2() |> IO.inspect()
