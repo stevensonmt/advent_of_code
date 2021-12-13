52e495f37d8e8ec8a61c2bc28bd5e02f509560d9
Matthew Stevenson <stevensonmt@gmail.com>
2021-12-12 19:11:26 -0800

    grrr part 2

--- 2021/day12/lib/day12.ex
+++ 2021/day12/lib/day12.ex
@@ -8,43 +8,67 @@ defmodule Day12 do
     |> String.split()
     |> Enum.map(&String.split(&1, "-"))
     |> Enum.reduce(%{}, fn [k, v], acc ->
-      IO.inspect({k, v})
-
       Map.update(acc, k, MapSet.new([v]), fn vs -> MapSet.put(vs, v) end)
       |> Map.update(v, MapSet.new([k]), fn vs -> MapSet.put(vs, k) end)
     end)
-    |> IO.inspect(label: "parsed")
   end
 
-  def build_paths(cave_system) do
-    build_paths(cave_system, ["start"])
+  def build_paths(cave_system, part_2?) do
+    build_paths(cave_system, ["start"], part_2?)
   end
 
-  defp build_paths(cave_system, [current_cave | prior_caves] = path) do
+  defp build_paths(cave_system, [current_cave | prior_caves] = path, part_2?) do
     cave_system
     |> Map.get(current_cave)
-    |> IO.inspect(label: "candidates")
-    |> Enum.flat_map(fn cave ->
+    |> Enum.reduce(MapSet.new(), fn cave, paths ->
       cond do
-        String.upcase(cave) == cave -> build_paths(cave_system, [cave | path])
-        cave in prior_caves -> [:dead_end]
-        cave == "end" -> [cave | path]
-        true -> build_paths(cave_system, [cave | path])
+        String.upcase(cave) == cave ->
+          # large caves are always valid next points
+          MapSet.union(paths, build_paths(cave_system, [cave | path], part_2?))
+
+        cave == "start" ->
+          paths
+
+        cave in prior_caves and small_only_once?(prior_caves, part_2?) ->
+          # small caves already visited can be revisited if no other small cave has been revisited
+          # IO.inspect({cave, prior_caves}, label: "why")
+          MapSet.union(paths, build_paths(cave_system, [cave | path], part_2?))
+
+        cave in prior_caves ->
+          # [:dead_end]
+          paths
+
+        cave == "end" ->
+          [cave | path] |> Enum.reverse() |> Enum.join(",") |> IO.inspect(label: "finished path")
+          MapSet.put(paths, [cave | path])
+
+        true ->
+          MapSet.union(paths, build_paths(cave_system, [cave | path], part_2?))
       end
     end)
-
-    # |> Enum.reject(&Kernel.==(Function.identity(&1), :dead_end))
-    # |> Enum.count()
   end
 
-  def do_pt_1(input) do
+  defp small_only_once?(_caves, false), do: false
+
+  defp small_only_once?(caves, true) do
+    caves
+    |> Enum.reject(fn cave -> String.upcase(cave) == cave end)
+    |> Enum.frequencies()
+    # |> IO.inspect(label: "frequencies check")
+    |> Enum.all?(fn {_, v} -> v == 1 end)
+
+    # |> IO.inspect()
+  end
+
+  def do_pt_1(input), do: solve(input)
+  def do_pt_2(input), do: solve(input, true)
+
+  defp solve(input, part_2? \\ false) do
     input
     |> parse()
-    |> IO.inspect()
-    |> build_paths()
-    |> IO.inspect(label: "paths")
-    |> Enum.count(&Kernel.==(&1, "end"))
-    |> IO.inspect(label: "just counting the ends")
+    |> build_paths(part_2?)
+    |> MapSet.size()
+    |> IO.inspect(label: "total paths")
   end
 end
 
@@ -56,7 +80,8 @@ b-d
 A-end
 b-end"
 
-Day12.do_pt_1(sample)
+# Day12.do_pt_1(sample)
+# Day12.do_pt_2(sample)
 
 sample2 = "dc-end
 HN-start
@@ -69,7 +94,8 @@ kj-sa
 kj-HN
 kj-dc"
 
-Day12.do_pt_1(sample2)
+# Day12.do_pt_1(sample2)
+Day12.do_pt_2(sample2)
 
 sample3 = "fs-end
 he-DX
@@ -90,8 +116,10 @@ zg-he
 pj-fs
 start-RW"
 
-Day12.do_pt_1(sample3)
+# Day12.do_pt_1(sample3)
+# Day12.do_pt_2(sample3)
 
-"input.txt"
-|> File.read!()
-|> Day12.do_pt_1()
+input = "input.txt" |> File.read!()
+
+# Day12.do_pt_1(input)
+# Day12.do_pt_2(input)
