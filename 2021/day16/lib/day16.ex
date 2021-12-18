c4d2e1308c5b16f87a5b27c7935849fb78464663
Matthew Stevenson <stevensonmt@gmail.com>
2021-12-18 11:58:25 -0800

    whygodwhy

--- 2021/day16/lib/day16.ex
+++ 2021/day16/lib/day16.ex
@@ -3,60 +3,74 @@ defmodule Day16 do
   My solution for `Day16`.
   """
 
+  @type literal :: integer
+  @type op :: %{version: integer, type: integer, length_type: integer, sub_packets: [packet]}
+  @type payload :: literal | op
+  @type packet :: %{version: integer, type: integer, payload: [payload]}
+
   def parse(input) do
     input
-    |> Integer.parse(16)
-    |> elem(0)
-    |> Integer.digits(2)
-    |> unpack()
+    |> Base.decode16!()
+    |> decode()
   end
 
-  def unpack(bits) do
-    bits
-    |> Enum.split(3)
-    |> packet_type()
+  @spec decode_packet(binary) :: packet
+  defp decode_packet(<<version::3, type::3, rest::bits>>) do
+    IO.inspect(binding(), label: "decode packet")
+    %{version: version, type: type, payload: unpack(type, rest, [])}
   end
 
-  def packet_type([version | bits]) do
-    bits
-    |> Enum.split(3)
-    |> literal_or_operator(version)
+  defp decode_packet(_n), do: <<>>
+
+  @spec decode_packets(integer, binary, [packet]) :: [packet]
+  defp decode_packets(0, rest, packets), do: {packets, rest}
+
+  defp decode_packets(n, bin, packets) do
+    {packet, rest} = decode_packet(bin)
+    decode_packets(n - 1, rest, [packet | packets])
   end
 
-  def literal_or_operator([type | bits], version) do
-    case Integer.undigits(type, 2) do
-      4 -> process_literals(version, type, bits)
-      _ -> process_operators(version, type, bits)
+  @spec decode_op(binary, [packet]) :: [packet]
+  defp decode_op(<<i::1, rest::bits>>, acc) do
+    case i do
+      0 ->
+        IO.inspect(rest)
+        <<len::15, bs::bits>> = rest
+        <<subs::bitstring-size(len), other::bits>> = bs
+        [decode_packets(subs) | acc]
+
+      # <<sub::len, other::bits>> = bs
+      # decode_packet(<<sub::len>>)
+
+      1 ->
+        <<total_packets::11, other::bits>> = rest
+        decode_packets(total_packets, other, []) ++ acc
     end
   end
 
-  def process_literals(version, type, bits) do
-    {version, type,
-     bits
-     |> Enum.chunk_every(5, :discard)
-     |> Enum.split_with(fn chnk -> hd(chnk) == 0 end)
-     |> realign()
-     |> decode_literals()}
+  defp unpack(4, bin, acc) do
+    {literal, rest} = decode_literal(bin, <<>>)
+    [decode_packet(rest) | [literal | acc]]
   end
 
-  def realign({literals, [last_lit | rest]}), do: {literals ++ [last_lit], List.flatten(rest)}
-
-  def decode_literals({literals, bits}) do
-    {literals
-     |> Enum.map(&tl(&1))
-     |> List.flatten()
-     |> Integer.undigits(2), bits}
-  end
-
-  def process_operators(version, type, [length_type | bits]) do
-    case length_type do 
-      0 -> bits |> Enum.split(15) |> decode_length_and_sub_packets() 
-      1 -> bits |> Enum.split(11) |> decode_num_sub_packets()
+  defp decode_literal(<<a::1, b::4, rest::bits>>, acc) do
+    case a do
+      0 -> {acc <> <<b>>, rest}
+      1 -> decode_literal(rest, acc <> <<b>>)
     end
+  end
+
+  defp unpack(_, <<v::3, t::3, rest::bits>>, acc) do
+    IO.inspect(binding(), label: "unpack")
+    {payload, rem} = decode_op(rest, [])
+    [decode_packet(rem) | [%{version: v, type: t, payload: payload} | acc]]
   end
 end
 
-"input.txt"
-|> File.read!()
+# "input.txt"
+# |> File.read!()
+# |> String.trim()
+# "D2FE28"
+"38006F45291200"
 |> Day16.parse()
 |> IO.inspect()
